3ca4f10a362ed4a23bbbdaf07555d9a574cf192a
CALLMELARE <zuoyunfei@twt.edu.cn>
2020-07-26 12:38:56 +0800

    fix navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,42 +1,55 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import StyledNavBar, { StyledMenuItem, MenuIcon, MenuItems } from './style';
-import Badge from 'components/Badge';
-import Avatar from 'components/Avatar';
-import face from 'assets/images/face.png';
-import { faCommentDots, faUsers, faFolder, faStickyNote, faEllipsisH, faCog } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import PropTypes from "prop-types";
+import StyledNavBar, { StyledMenuItem, MenuIcon, MenuItems } from "./style";
+import Badge from "components/Badge";
+import Avatar from "components/Avatar";
 
-const NavBar = ({ children, ...rest }) => {
-    return (
-        <StyledNavBar {...rest}>
-            <Avatar src={face} status="online" />
-            <MenuItems>
-                <MenuItem icon={faCommentDots} showBadge active />
-                <MenuItem icon={faUsers} />
-                <MenuItem icon={faFolder} />
-                <MenuItem icon={faStickyNote} />
-                <MenuItem icon={faEllipsisH} />
-                <MenuItem icon={faCog} css={`align-self:end`}/>
-            </MenuItems>
-        </StyledNavBar>
-    );
-};
+import profileImage from "assets/images/face.png";
+import {
+  faCommentDots,
+  faUsers,
+  faFolder,
+  faStickyNote,
+  faEllipsisH,
+  faCog,
+} from "@fortawesome/free-solid-svg-icons";
 
-function MenuItem({ icon, active, showBadge, ...rest }) {
-    return (
-        <StyledMenuItem active={active} {...rest}>
-            <a href="#">
-                <Badge show={showBadge}>
-                    <MenuIcon active={active} icon={icon} />
-                </Badge>
-            </a>
-        </StyledMenuItem>
-    )
+import "styled-components/macro";
+
+function NavBar({ ...rest }) {
+  return (
+    <StyledNavBar {...rest}>
+      <Avatar src={profileImage} status="online" />
+      <MenuItems>
+        <MenuItem showBadge active icon={faCommentDots} />
+        <MenuItem icon={faUsers} />
+        <MenuItem icon={faFolder} />
+        <MenuItem icon={faStickyNote} />
+        <MenuItem icon={faEllipsisH} />
+        <MenuItem
+          icon={faCog}
+          css={`
+            align-self: end;
+          `}
+        />
+      </MenuItems>
+    </StyledNavBar>
+  );
 }
 
-NavBar.propTypes = {
-    
-};
+function MenuItem({ icon, active, showBadge, ...rest }) {
+  return (
+    <StyledMenuItem active={active} {...rest}>
+      <a href="#">
+        <Badge show={showBadge}>
+          <MenuIcon active={active} icon={icon} />
+        </Badge>
+      </a>
+    </StyledMenuItem>
+  );
+}
+
+NavBar.propTypes = {};
 
 export default NavBar;
 
--- src/components/NavBar/navBar.stories.js
+++ src/components/NavBar/navBar.stories.js
@@ -1,19 +1,24 @@
 import React from "react";
 import NavBar, { MenuItem } from ".";
-import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
+import { faCommentDots } from "@fortawesome/free-solid-svg-icons";
 import "styled-components/macro";
 
 export default {
-    title: "页面组件/NavBar",
-    component: NavBar
-}
+  title: "页面组件/NavBar(侧边栏)",
+  component: NavBar,
+};
 
 export const Default = () => <NavBar />;
 
 export const Menu = () => {
-    return (
-        <div css={`background-color:${({ theme }) => theme.darkPurple};width:100px`} >
-            <MenuItem showBadge active icon={faCommentDots} />
-        </div>
-    )
-}
+  return (
+    <div
+      css={`
+        background-color: ${({ theme }) => theme.darkPurple};
+        width: 100px;
+      `}
+    >
+      <MenuItem showBadge active icon={faCommentDots} />
+    </div>
+  );
+};
