1781afb2eda49726a07ae9af33dfe0174d34a36b
CALLMELARE <zuoyunfei@twt.edu.cn>
2020-07-26 15:41:22 +0800

    Footer

---
+++ src/components/Footer/footer.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from ".";
+
+export default {
+  title: "页面组件/Footer(底部组件)",
+  component: Footer,
+};
+
+export const Default = () => (
+  <div style={{ marginTop: 80 }}>
+    <Footer />
+  </div>
+);
