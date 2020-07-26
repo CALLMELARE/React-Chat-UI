fa37d72cd8f56c969677b9b24df3334a6b89ef7e
CALLMELARE <zuoyunfei@twt.edu.cn>
2020-07-26 15:23:53 +0800

    Update chatBubble.stories.js

--- src/components/ChatBubble/chatBubble.stories.js
+++ src/components/ChatBubble/chatBubble.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ChatBubble from ".";
+import VoiceMessage from "components/VoiceMessage";
 
 export default {
   title: "UI 组件/ChatBubble(消息气泡)",
@@ -14,5 +15,17 @@ export const FromOthers = () => (
 export const Mine = () => (
   <ChatBubble type="mine" time="昨天 下午16：30">
     这是一条我自己发送的聊天消息
+  </ChatBubble>
+);
+
+export const VoiceMessageType = () => (
+  <ChatBubble time="昨天 下午18：30">
+    <VoiceMessage time="01:24" />
+  </ChatBubble>
+);
+
+export const VoiceMessageTypeMine = () => (
+  <ChatBubble type="mine" time="昨天 下午18：30">
+    <VoiceMessage type="mine" time="01:24" />
   </ChatBubble>
 );
