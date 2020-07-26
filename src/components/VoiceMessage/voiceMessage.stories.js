808c690b8c0e2d503a0823a6af6f0f2b4716f319
CALLMELARE <zuoyunfei@twt.edu.cn>
2020-07-26 15:22:22 +0800

    VoiceMessage

---
+++ src/components/VoiceMessage/voiceMessage.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import VoiceMessage from ".";
+
+export default {
+  title: "UI 组件/VoiceMessage(语音消息)",
+  component: VoiceMessage,
+};
+
+export const Default = () => <VoiceMessage time="01:25" />;
