ee96d9b485499beb26d349ac2c41cdf87748fda5
CALLMELARE <zuoyunfei@twt.edu.cn>
2020-07-27 11:31:53 +0800

    Switch

---
+++ src/components/Switch/switch.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Switch from ".";
+
+export default {
+  title: "UI 组件/Switch(开关)",
+  component: Switch,
+};
+
+export const Default = () => (
+  <div style={{ padding: "1vw" }}>
+    <Switch />
+  </div>
+);
