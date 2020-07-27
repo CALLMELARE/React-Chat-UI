39561669c4350566609a02fe836e22197b578c8c
CALLMELARE <zuoyunfei@twt.edu.cn>
2020-07-27 11:16:55 +0800

    update

---
+++ src/components/Profile/profile.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Profile from ".";
+
+export default {
+  title: "页面组件/Profile(个人信息)",
+  component: Profile,
+};
+
+export const Default = () => <Profile children="children" />;
